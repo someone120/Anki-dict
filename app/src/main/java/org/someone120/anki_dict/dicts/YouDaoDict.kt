15126ca24e1c564fc0466e373c68c145fa765f04
someone120 <mouren1101@outlook.com>
2023-09-03 13:59:58 +0800

    feat: add YouDaoDict provider

---
+++ app/src/main/java/org/someone120/anki_dict/dicts/YouDaoDict.kt
@@ -0,0 +1,11 @@
+package org.someone120.anki_dict.dicts
+
+class YouDaoDict:Dictionary{
+    override fun getParaphrase(word: String): List<Paraphrase> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getExampleSentence(word: String): List<ExampleSentence> {
+        TODO("Not yet implemented")
+    }
+}
