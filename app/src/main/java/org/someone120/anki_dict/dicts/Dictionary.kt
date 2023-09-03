c6b69306b1207974915393a179b4104fd210f85e
someone120 <mouren1101@outlook.com>
2023-09-03 14:19:24 +0800

    docs: add docs for dict

--- app/src/main/java/org/someone120/anki_dict/dicts/Dictionary.kt
+++ app/src/main/java/org/someone120/anki_dict/dicts/Dictionary.kt
@@ -1,9 +1,25 @@
 package org.someone120.anki_dict.dicts
 
 interface Dictionary {
-    fun getParaphrase(word:String):List<Paraphrase>
+    /***
+     * Get Paraphrase of this word.
+     * @param word
+     * @return definition
+    */
+    fun getDefinition(word:String):List<Definition>
     fun getExampleSentence(word:String):List<ExampleSentence>
 }
 
-data class Paraphrase(val pos:String,val paraphrase:String)
+/***
+ * the definition of word.
+ * @param pos part of speech (noun, verb, adjective etc).
+ * @param definition
+ */
+data class Definition(val pos:String, val definition:String)
+
+/***
+ * Example sentence.
+ * @param origin the sentence written by origin language.
+ * @param translation the translation of this sentence.
+ */
 data class ExampleSentence(val origin:String,val translation:String)
