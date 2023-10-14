52ff32ca2f8641e786c97060d1b80bb4c8b05d6b
someone120 <mouren1101@outlook.com>
2023-10-15 01:37:45 +0800

    feat: add appScope on application life

--- app/src/main/java/org/someone120/anki_dict/MainActivity.kt
+++ app/src/main/java/org/someone120/anki_dict/MainActivity.kt
@@ -3,15 +3,9 @@ package org.someone120.anki_dict
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import org.someone120.anki_dict.ui.theme.AnkidictTheme
+import kotlinx.coroutines.MainScope
 
+val appScope by lazy { MainScope() }
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
